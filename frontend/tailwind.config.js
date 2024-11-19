4b1bdc61432e4359703fcbb81f9cd5d87dd06d5e
yahia <rakifullstack@gmail.com>
2024-11-19 15:57:02 +0100

    2- UI completed

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,30 @@
+import daisyui from "daisyui"
+
+import daisyUIThemes from "daisyui/src/theming/themes";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{html,js}',
+      './components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+   daisyui
+  ],
+  daisyui: {
+		themes: [
+			"light",
+			{
+				black: {
+					...daisyUIThemes["black"],
+					primary: "rgb(29, 155, 240)",
+					secondary: "rgb(24, 24, 24)",
+				},
+			},
+		],
+	},
+}
